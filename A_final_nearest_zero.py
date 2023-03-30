70ffaeb12afd1cc9ef70a4fd215238bf4dbc8d50
Андрей Николаевич <andreykaspb@gmail.com>
2023-03-30 13:02:21 +0300

    modified:   A_final_nearest_zero.py 	modified:   B_final_sleight_of_hand.py

--- A_final_nearest_zero.py
+++ A_final_nearest_zero.py
@@ -1,19 +1,38 @@
 # A. Ближайший ноль
-# ID успешного решения - 
+# ID успешного решения - 84841407
 
 from typing import List, Tuple
 
+
 def read_input() -> Tuple[List[int], int]:
     """Считывает ввод в переменные."""
-    n = int(input())
+    number_plots = int(input())
     houses = list(map(int, input().strip().split()))
-    return houses, n
+    return houses, number_plots
 
 
-def main():
-    houses, n = read_input()
-    print(" ".join(map(str, get_distance(houses, n))))
+def get_distance(houses: List[str], number_plots: int) -> List[int]:
+    """Высчитывает дистанцию каждого из участков до ближайшего нулевого участка."""
+    distanse = [number_plots] * number_plots
+    zero = [i for i in range(number_plots) if houses[i] == 0]
+    first_zero = zero[0]
+    last_zero = zero[-1]
+
+    for i in range(first_zero, number_plots):
+        if houses[i] != 0:
+            distanse[i] = distanse[i - 1] + 1
+        else:
+            distanse[i] = 0
+    for i in range(last_zero, first_zero, -1):
+        if houses[i] != 0:
+            distanse[i] = min(distanse[i], distanse[i + 1] + 1)
+        else:
+            distanse[i] = 0
+    for i in range(first_zero - 1, -1, -1):
+        distanse[i] = distanse[i + 1] + 1
+    return distanse
 
 
 if __name__ == '__main__':
-    main()
+    houses, number_plots = read_input()
+    print(" ".join(map(str, get_distance(houses, number_plots))))
--- B_final_sleight_of_hand.py
+++ B_final_sleight_of_hand.py
@@ -1,5 +1,5 @@
 # B. Ловкость рук
-# ID успешного решения - 
+# ID успешного решения - 84839306
 
 from typing import List, Tuple
 
@@ -9,7 +9,27 @@ BOYS = 2
 
 
 def read_input() -> Tuple[int, List[str]]:
-    number = int(input())
+    """Считывает ввод в переменные."""
+    boymaxkey = int(input())
     matrix = [str(input()) for i in range(MATRIX_SIZE)]
-    return number, matrix
+    return boymaxkey, matrix
 
+
+def trainer(boymaxkey: int, matrix: List[str]) -> int:
+    """Проводим подсчёт результатов игры."""
+    numbers = {a: 0 for a in range(1, 10)}
+    scores = 0
+    for i in range(MATRIX_SIZE):
+        for k in matrix[i]:
+            if k == '.':
+                continue
+            numbers[int(k)] += 1
+    for i in numbers.values():
+        if 0 < i <= 2 * boymaxkey:
+            scores += 1
+    return scores
+
+
+if __name__ == '__main__':
+    boymaxkey, matrix = read_input()
+    print(trainer(boymaxkey, matrix))
